7dff7c4da10ec1254b1d4b76a61a39b5f3fcb6dc
LukeVanLaarhoven <60391960+LukeVanLaarhoven@users.noreply.github.com>
2021-11-03 09:37:38 +0100

    completed weapon script

--- Assets/Scripts/Weapons/Weapons.cs
+++ Assets/Scripts/Weapons/Weapons.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class Weapons : MonoBehaviour
 {
+    private float initialRateOfFire;
     public float rateOfFire;
     public float weaponSpread;
     public float bulletSpeed;
 
     public GameObject barrel;
-    private GameObject bulletPrefab;
+    public GameObject bulletPrefab;
+    private GameObject bullet;
+
+    List<GameObject> bulletList = new List<GameObject>();
+
+    public void Start()
+    {
+        initialRateOfFire = rateOfFire;
+    }
 
     public void Shoot()
     {
+        rateOfFire -= Time.deltaTime;
+
         if (Input.GetButton("Fire1") && rateOfFire < 0)
         {
-            print("Shoot");
+            Debug.Log("Shoot");
+
+            bullet = Instantiate(bulletPrefab, barrel.transform.position, CalculateBulletSpread(weaponSpread));
+            bulletList.Add(bullet);
+
+            rateOfFire = initialRateOfFire;
         }
+
+        for (int i = 0; i < bulletList.Count; i++)
+        {
+            bulletList[i].transform.Translate(bulletSpeed * Time.deltaTime, 0, 0);
+        }
+
+        Destroy(bullet, 5);
+
+        if(bullet == null)
+        {
+            bulletList.Remove(bullet);
+        }
+
+        Debug.Log(bulletList.Count);
     }
 
-    private void CalculateBulletSpread()
+    private Quaternion CalculateBulletSpread(float weaponSpread)
     {
-        Quaternion angle = Quaternion.Euler(0, 0, weaponSpread);
+        Quaternion angle = Quaternion.Euler(0, 0, Random.Range(-weaponSpread, weaponSpread));
+
+        return angle;
     }
 }
